a4931fc021d8ba5dc156e06852e4ab8a1d3d62fa
Ramani-Narasimha <thunuguntlaramani@gmail.com>
2022-12-08 16:42:36 +0530

    learned about redux

---
+++ src/SendingData.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import axios from 'axios'
+class SendingData extends React.Component{
+	constructor(){
+		super();
+		this.state={
+			username:"",
+			password:""
+		}
+	}
+	handleChange=(e)=>{
+		this.setState({
+			[e.target.name]:e.target.value
+		})
+	}
+	handleClick=()=>{
+			console.log(this.state.username,this.state.password)
+			axios.post("http://localhost:4000/register",
+			{email:this.state.username,
+				passcode:this.state.password})
+			this.setState({
+				username:"",
+				password:""
+			})
+		}
+		render(){
+			return(
+				<div>
+					<p><input
+						value={this.state.username}
+						placeholder="Enter username"
+						onChange={this.handleChange}
+						name="username"/></p>
+					<p><input
+						value={this.state.password}
+						placeholder="Enter password"
+						onChange={this.handleChange}
+						name="password"/></p>
+						<button onClick={this.handleClick}>send</button>
+				</div>
+			)
+		}
+	}
+
+export default SendingData
